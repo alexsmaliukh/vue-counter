2e4bd9bbad3ae83ecf755f983c1187700d18ee81
unknown <sasha.smalyux@gmail.com>
2019-11-21 23:07:27 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h2>addition = {{ counter }}</h2>
+    <button v-on:click="riseCounter">like</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 10
+    };
+  },
+  methods: {
+    riseCounter: function() {
+      if (this.counter > 0) {
+        this.counter--;
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
